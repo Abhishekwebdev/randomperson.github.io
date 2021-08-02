acb42163665706382a0b7e2bb1330e0a7b0bc4fe
Abhishek Patel <Abhi882731@gmail.com>
2021-08-02 06:12:01 -0700

    Updates

---
+++ precache-manifest.4005924d5a9a9e66ac23d0c596ea6cfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0119cf640b3be3df4e0d477e008c13",
+    "url": "/randomperson.github.io/index.html"
+  },
+  {
+    "revision": "72d464a89c31122edf65",
+    "url": "/randomperson.github.io/static/css/main.5921d42c.chunk.css"
+  },
+  {
+    "revision": "2749cf3d00a3cdbd8255",
+    "url": "/randomperson.github.io/static/js/2.5e6cb5f4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/randomperson.github.io/static/js/2.5e6cb5f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d464a89c31122edf65",
+    "url": "/randomperson.github.io/static/js/main.e7fbaf73.chunk.js"
+  },
+  {
+    "revision": "ac4509f2b44bb08e3e66",
+    "url": "/randomperson.github.io/static/js/runtime-main.08037714.js"
+  }
+]);
